56dc58c67fb628742c22b2e17409e19cfca4e137
KoynovStas <skojnov@yandex.ru>
2016-08-22 15:21:00 +0300

    Tests: list.h - add test_list_is_first

--- tests/list_tests.c
+++ tests/list_tests.c
@@ -45,9 +45,49 @@ int test_list_empty(struct test_info_t  *test_info)
 
 
 
+int test_list_is_first(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    DECLARE_LIST_HEAD(tmp_list);
+
+    struct tmp_data d1, d2, d3;
+
+    list_push_front(&d1.list, &tmp_list);   //now d1 is first
+
+    if(!list_is_first(&d1.list, &tmp_list)) //d1 must be first
+        return TEST_BROKEN;
+
+
+    if(list_is_first(&d2.list, &tmp_list))  //d2 is not first (d2 out of the list.)
+        return TEST_BROKEN;
+
+
+
+    list_push_front(&d2.list, &tmp_list);    //now d2 is first
+
+    if(!list_is_first(&d2.list, &tmp_list))  //d2 must be first
+        return TEST_BROKEN;
+
+
+    if(list_is_first(&d1.list, &tmp_list))   //d1 is not first (d1 is second)
+        return TEST_BROKEN;
+
+
+    if(list_is_first(&d3.list, &tmp_list))   //d3 is not first (d3 out of the list.)
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
     test_list_empty,
+    test_list_is_first,
 };
 
 
