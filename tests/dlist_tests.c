4adf6860ce53d71d7879c52187ccf8aaab4ad19f
KoynovStas <skojnov@yandex.ru>
2016-08-22 16:37:44 +0300

    Add src unit_test for dlist

---
+++ tests/dlist_tests.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+#include "unit_tests.h"
+#include "dlist.h"
+
+
+
+
+
+ptest_func tests[] =
+{
+
+};
+
+
+
+int main(void)
+{
+
+    RUN_TESTS(tests);
+
+    return 0;
+}
+
