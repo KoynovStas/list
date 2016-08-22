5a8d529b5213f87b52a00eef6fbfd2ddafa8dd92
KoynovStas <skojnov@yandex.ru>
2016-08-22 14:02:57 +0300

    Add src unit_test

---
+++ tests/list_tests.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+#include "unit_tests.h"
+#include "list.h"
+
+
+
+
+
+
+
+
+
+ptest_func tests[] =
+{
+
+};
+
+
+
+int main(void)
+{
+
+    RUN_TESTS(tests);
+
+    return 0;
+}
+
