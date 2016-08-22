3e1e8e2b821f2e0c7abe3423aeaede9f100ef3a9
KoynovStas <skojnov@yandex.ru>
2016-08-22 15:20:06 +0300

    Tests: list.h - add test_list_empty

--- tests/list_tests.c
+++ tests/list_tests.c
@@ -11,13 +11,43 @@
 
 
 
+struct tmp_data
+{
+   struct list_head list;
+   int              data;
+};
 
+
+
+
+
+int test_list_empty(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    DECLARE_LIST_HEAD(tmp_list);
+    struct tmp_data d1;
+
+
+    if(!list_empty(&tmp_list))              //list must be empty
+        return TEST_BROKEN;
+
+
+    list_push_front(&d1.list, &tmp_list);   //now d1 is first
+
+    if(list_empty(&tmp_list))               //now list is NOT empty
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
 
 
 
 ptest_func tests[] =
 {
-
+    test_list_empty,
 };
 
 
