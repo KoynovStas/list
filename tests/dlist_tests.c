17bb287080160241ded8f7fdcf83214d41b1053d
KoynovStas <skojnov@yandex.ru>
2016-08-22 16:48:16 +0300

    Tests: dlist.h - add test_list_empty

--- tests/dlist_tests.c
+++ tests/dlist_tests.c
@@ -11,9 +11,45 @@
 
 
 
-ptest_func tests[] =
+struct tmp_data
+{
+   struct dlist_head list;
+   int               data;
+};
+
+
+
+
+
+int test_list_empty(struct test_info_t  *test_info)
 {
 
+    TEST_INIT;
+
+    DECLARE_DLIST_HEAD(tmp_list);
+    struct tmp_data d1;
+
+
+    if(!dlist_empty(&tmp_list))             //list must be empty
+        return TEST_BROKEN;
+
+
+    dlist_push_front(&d1.list, &tmp_list);   //now d1 is first
+
+    if(dlist_empty(&tmp_list))               //now list is NOT empty
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+
+
+ptest_func tests[] =
+{
+    test_list_empty,
 };
 
 
