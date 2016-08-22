7f3a4aff60a499c3218b92b897f70f40c764546c
KoynovStas <skojnov@yandex.ru>
2016-08-22 17:34:12 +0300

    dlist.h: add dlist_last_data_or_null

--- dlist.h
+++ dlist.h
@@ -537,6 +537,21 @@ static inline void dlist_splice_back(struct dlist_head *list,
 
 
 
+/*
+ * dlist_last_data_or_null - get the last struct (data) from a list
+ *
+ * head:   the list head to take the element from.
+ * type:   the type of the struct of data this is embedded in.
+ * member: the name of the node(dlist_head) within the struct of data.
+ *
+ * Note it returns NULL if the list(node) is empty.
+ */
+#define dlist_last_data_or_null(head, type, member) \
+    (!dlist_empty(head) ? dlist_last_data(head, type, member) : NULL)
+
+
+
+
 
 
 
