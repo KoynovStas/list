bdb3a80dabb548c3ed9a82d5da67efe78d78b573
KoynovStas <skojnov@yandex.ru>
2016-08-23 11:23:38 +0300

    Tests:dlist.h - add test_dlist_max

--- tests/dlist_tests.c
+++ tests/dlist_tests.c
@@ -1237,6 +1237,55 @@ int test_dlist_min(struct test_info_t  *test_info)
 
 
 
+int test_dlist_max(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    DECLARE_DLIST_HEAD(tmp_list);
+
+    const size_t COUNT_NODES = 100;
+    size_t i;
+    struct tmp_data  nodes[COUNT_NODES];
+    struct tmp_data  *tmp_data;
+    struct dlist_head *max;
+
+
+    for(i=0; i < COUNT_NODES; i++)
+    {
+        nodes[i].data = i;
+        dlist_push_back(&nodes[i].list, &tmp_list);
+    }
+
+
+    max = dlist_max(tmp_list.next, &tmp_list, comp_less);
+    tmp_data = dlist_data(max, struct tmp_data, list);
+
+    if(tmp_data->data != COUNT_NODES-1)
+        return TEST_BROKEN;
+
+
+    dlist_init_head(&tmp_list);  //clear
+
+    for(i=0; i < COUNT_NODES; i++)
+    {
+        nodes[i].data = -i;
+        dlist_push_back(&nodes[i].list, &tmp_list);
+    }
+
+
+    max = dlist_max(tmp_list.next, &tmp_list, comp_less);
+    tmp_data = dlist_data(max, struct tmp_data, list);
+
+    if(tmp_data->data != 0)
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
     test_list_empty,
@@ -1273,6 +1322,7 @@ ptest_func tests[] =
     //Algorithm
     test_list_for_each,
     test_dlist_min,
+    test_dlist_max,
 };
 
 
