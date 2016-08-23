19080adaa3a6dfdba96a18750d1b87b9b9f5269c
KoynovStas <skojnov@yandex.ru>
2016-08-23 11:13:27 +0300

    Tests:dlist.h - add test_dlist_min

--- tests/dlist_tests.c
+++ tests/dlist_tests.c
@@ -1179,6 +1179,64 @@ int test_list_for_each(struct test_info_t  *test_info)
 
 
 
+int comp_less(const struct dlist_head *n1,const struct dlist_head *n2)
+{
+    struct tmp_data *d1 = dlist_data(n1, struct tmp_data, list);
+    struct tmp_data *d2 = dlist_data(n2, struct tmp_data, list);
+
+    return d1->data < d2->data;
+}
+
+
+int test_dlist_min(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    DECLARE_DLIST_HEAD(tmp_list);
+
+    const size_t COUNT_NODES = 100;
+    size_t i;
+    struct tmp_data  nodes[COUNT_NODES];
+    struct tmp_data  *tmp_data;
+    struct dlist_head *min;
+
+
+    for(i=0; i < COUNT_NODES; i++)
+    {
+        nodes[i].data = i;
+        dlist_push_back(&nodes[i].list, &tmp_list);
+    }
+
+
+    min = dlist_min(tmp_list.next, &tmp_list, comp_less);
+    tmp_data = dlist_data(min, struct tmp_data, list);
+
+    if(tmp_data->data != 0)
+        return TEST_BROKEN;
+
+
+    dlist_init_head(&tmp_list);  //clear
+
+    for(i=0; i < COUNT_NODES; i++)
+    {
+        nodes[i].data = -i;
+        dlist_push_back(&nodes[i].list, &tmp_list);
+    }
+
+
+    min = dlist_min(tmp_list.next, &tmp_list, comp_less);
+    tmp_data = dlist_data(min, struct tmp_data, list);
+
+    if(tmp_data->data != -(COUNT_NODES-1))
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
     test_list_empty,
@@ -1214,6 +1272,7 @@ ptest_func tests[] =
 
     //Algorithm
     test_list_for_each,
+    test_dlist_min,
 };
 
 
